efeca8069aaad6121d137102bb5e1fae7c4e1695
thomas <t.lacan@useradgents.com>
2019-04-25 11:15:54 +0700

    Begin project

---
+++ MyJogs/MyJogs/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  MyJogs
+//
+//  Created by thomas lacan on 25/04/2019.
+//  Copyright © 2019 thomas lacan. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MyJogs/MyJogs/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  MyJogs
+//
+//  Created by thomas lacan on 25/04/2019.
+//  Copyright © 2019 thomas lacan. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
