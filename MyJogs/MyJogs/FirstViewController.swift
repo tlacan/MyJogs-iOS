aeece0ff92dc3dbcd593f629d2a81d2de7f11dff
thomas <t.lacan@useradgents.com>
2019-04-30 14:45:15 +0700

    Configure Crashlytics

--- MyJogs/MyJogs/FirstViewController.swift
+++ MyJogs/MyJogs/FirstViewController.swift
@@ -7,14 +7,21 @@
 //
 
 import UIKit
+import Crashlytics
 
 class FirstViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let button = UIButton(type: .roundedRect)
+        button.frame = CGRect(x: 20, y: 50, width: 100, height: 30)
+        button.setTitle("Crash", for: [])
+        button.addTarget(self, action: #selector(self.crashButtonTapped(_:)), for: .touchUpInside)
+        view.addSubview(button)
     }
 
-
+    @IBAction func crashButtonTapped(_ sender: AnyObject) {
+        Crashlytics.sharedInstance().crash()
+    }
 }
 
